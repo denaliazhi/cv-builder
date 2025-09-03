ae49a26d2d557bb57c38d93a470801ce6c1b9f22
Denalia Zhi <d.naliazhi@gmail.com>
2025-09-03 15:40:02 -0400

    Fix date format to short month + year

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -6,7 +6,6 @@ export function Entry({ title, subtitle, dates, children }) {
         <p>{subtitle}</p>
       </div>
       <p>
-        {/* TO DO: only shows short month, missing year */}
         {dates[0]
           ? `${formatDate(dates[0])} - ${
               dates[1] ? formatDate(dates[1]) : "Present"
@@ -19,7 +18,6 @@ export function Entry({ title, subtitle, dates, children }) {
 }
 
 function formatDate(date) {
-  return new Date(date).toLocaleString("default", {
-    month: "short",
-  });
+  const options = { month: "short", year: "numeric" };
+  return new Date(date).toLocaleString("default", options);
 }
