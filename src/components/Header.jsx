5c7a12f45948f436d92a83b52ed747a9af79a680
Denalia Zhi <d.naliazhi@gmail.com>
2025-09-03 21:28:57 -0400

    Fix header rendering error due to form not existing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,15 @@ export function Header({ content }) {
   const obj = content[0];
   return (
     <div className="cv-header">
-      <h2>{obj.name.value}</h2>
-      <div>
-        <p>{obj.email.value}</p>
-        <p>{obj.phone.value}</p>
-      </div>
+      {obj && (
+        <>
+          <h2>{obj.name.value}</h2>
+          <div>
+            <p>{obj.email.value}</p>
+            <p>{obj.phone.value}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
